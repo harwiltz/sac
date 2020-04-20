b5ab9d44603cb6c8acb383bb361ec5129ad4dd29
Harley Wiltzer <harley.wiltzer@mail.mcgill.ca>
2020-04-20 18:08:35 -0400

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sac',
+    version='0.0.1',
+    author='Harley Wiltzer',
+    author_email='harley.wiltzer@mail.mcgill.ca',
+    description='Implementation of Soft Actor Critic',
+    license='GPL',
+    keywords='reinforcement learning soft actor critic',
+    packages=['sac']
+)
