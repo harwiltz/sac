Attempt to fix in-place gradient anomaly

sac/nets.py
@@ -22,7 +22,7 @@ class ValueNetwork(nn.Module):
     def forward(self, x):
         x = F.relu(self._l1(x))
         x = F.relu(self._l2(x))
-        x = self._l3(x)
+        x = self._l3(x.clone())
         return x
 
     def exponential_smooth(self, other, tau):
@@ -49,7 +49,7 @@ class DiscreteCriticNetwork(nn.Module):
     def forward(self, s, a):
         s = F.relu(self._l1(s))
         s = F.relu(self._l2(s))
-        s = self._l3(s)
+        s = self._l3(s.clone())
         return s.gather(1, a.long())
 
 class ActorNetwork(nn.Module):